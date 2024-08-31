06576b3bb1305d2374d9bc8cd94273be13d82f88
Farman Ahmed <farmanahmed888@gmail.com>
2024-08-31 11:16:20 +0000

    project

---
+++ api/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
+
+const app = express();
+
+app.use(cors({
+ origin: 'http://localhost:5173',
+  credentials: true
+}));
+// Middleware for parsing JSON and handling cookies
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
+const server = app.listen(8012, () => {
+  console.log("Server is running on port 8012");
+});
+
+export { app, server };
